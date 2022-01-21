f6faf71072ee1cf107d381771333e443dcbdfb49
Claudio Gabriel Flores <claudiofloresty@gmail.com>
2022-01-21 16:12:56 -0300

    Añadiendo componente GifGridItems y conectando a los demas componentes

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,33 +1,50 @@
 //Coleccion de todos los elementos 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { GifGridItem } from './GifGridItem';
 
-export const GifGrid = ({category}) => {
-  
-  const getGifs = async() => {
-      const apiKey = 'qKbTiiVJ7JNUr3ptSRRl8ocadgJEQtoS';
-      const query = 'Boku no hero';
-      const limit = '10';
-      const url = `https://api.giphy.com/v1/gifs/search?q=${query.replace(/\s+/g, '+')}&limit=${limit}&api_key=${apiKey}`;
-      const resp = await fetch(url);
-      const {data} = await resp.json();
+export const GifGrid = ({ category }) => {
 
-      const gifs = data.map( img => {
-          return {
-              id: img.id,
-              title: img.title,
-              url: img.images?.downsized_medium.url
-          }
-      })
-      console.log(gifs);
-      //console.log(data);
+    const [images, setImages] = useState([]);
 
-  }
+    //Hace que se ejecute la funcion cuando el componente es renderizado por primera vez
+    useEffect(() => {
+        getGifs();
+    }, []);
 
-  getGifs();
-  
+
+    const getGifs = async () => {
+        const apiKey = 'qKbTiiVJ7JNUr3ptSRRl8ocadgJEQtoS';
+        const query = 'Boku no hero';
+        const limit = '10';
+        const url = `https://api.giphy.com/v1/gifs/search?q=${query.replace(/\s+/g, '+')}&limit=${limit}&api_key=${apiKey}`;
+        const resp = await fetch(url);
+        const { data } = await resp.json();
+
+        const gifs = data.map(img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        })
+        setImages(gifs);
+    }
+
+    //getGifs();
+
     return (
-      <>
-        <h3>{category}</h3>
-      </>
-  );
+        <>
+            <h3>{category}</h3>
+            <ol>
+                {
+                    images.map(img => (
+                        <GifGridItem
+                            key={img.id}
+                            {...img}
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    );
 };
