0b2ca69e8c89ab1eaa89bbb03e4792b6030b5e63
Claudio Gabriel Flores <claudiofloresty@gmail.com>
2022-01-21 17:19:26 -0300

    Optimizacion de custom hook

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,31 +1,18 @@
 //Coleccion de todos los elementos 
 import React from 'react';
 import { useFetchGifs } from '../hooks/useFetchGifs';
-//import React, { useEffect, useState } from 'react';
-//import { getGifs } from '../helpers/getGifs';
-//import { GifGridItem } from './GifGridItem';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const {data, loading} = useFetchGifs();
-    console.log(data);
-    console.log(loading);
-    /* const [images, setImages] = useState([]);
-    
-    //Hace que se ejecute la funcion cuando el componente es renderizado por primera vez
-    useEffect(() => {
-        getGifs(category)
-            .then(setImages)
-            .catch(console.log)
-    }, [category]); */
-
+    const {data:images, loading} = useFetchGifs(category);
 
 
     return (
         <>
             <h3>{category}</h3>
-            {loading ? 'Cargando...' : 'Data cargada'}
-            {/* <div className='card-grid'>
+            {loading && <p>Loading...</p>}
+            <div className='card-grid'>
                 {
                     images.map(img => (
                         <GifGridItem
@@ -34,7 +21,7 @@ export const GifGrid = ({ category }) => {
                         />
                     ))
                 }
-            </div> */}
+            </div>
         </>
     );
 };
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,15 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {getGifs} from '../helpers/getGifs';
+;
 
-export const useFetchGifs = () => {
+
+export const useFetchGifs = ( category ) => {
     
     const [state, setstate] = useState({
         data: [],
         loading: true
     });
 
-    setTimeout(() => {
-        
-    })
+    //Hace que se ejecute la funcion cuando el componente es renderizado por primera vez
+    useEffect(()=>{
+        getGifs(category)
+            .then( imgs => {
+                /* setTimeout(() => {
+                    setstate({
+                        data: imgs,
+                        loading: false
+                    })    
+                }, 3000) */
+
+                setstate({
+                    data: imgs,
+                    loading: false
+                })
+            });
+    });
+
     return state; // {data, loading}
 
 };
